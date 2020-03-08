7198e38da3f179f98078a6ccd0f510a91fcf5288
z0f1b <35045470+z0f1b@users.noreply.github.com>
2020-03-08 09:58:17 -0700

    connected server and client

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React, {useState, Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PersonList from './components/PersonList';
+import PersonInput from './components/PersonInput';
+import axios from 'axios';
+import PersonListAll from './components/PersonList';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import FindConnection from './components/FindConnection';
+import AddConnection from './components/AddConnection';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state  = {
+      persons: []
+    };
+  }
+
+  // componentDidMount() {
+  //   this.callBackendAPI()    
+  // }
+
+  // callBackendAPI = () => {
+  //   axios.get('/persons')   
+  //           .then( res => {
+  //               this.setState({persons: res.data.persons});
+  //           })
+  //           .catch(err => console.log(err));
+  // };
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          {/* <PersonList dataFromParent = {this.state.persons}/> */}
+          <div class="buttons">
+            <Link to="/addConnection">
+              <button type="button" class="btn btn-lg">
+                <div style={{color: "#FFFFFF"}}>Add a new connection</div>
+              </button>
+            </Link>
+            <Link to="/findConnection">
+              <button type="button" class="btn btn-lg">
+                <div style={{color: "#FFFFFF"}}>Find a connection</div>
+              </button>
+            </Link>
+          </div>
+          <Switch>
+            <Route exact path="/addConnection" component={AddConnection} />
+            <Route path="/findConnection" component={FindConnection} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );  
+  };
+}
+
+export default App;
--- server/routes/persons.js
+++ server/routes/persons.js
@@ -22,6 +22,40 @@ router.get('/:personId', async (req, res) => {
     }
 });
 
+router.get('/findconnection', async function(req, res) {
+    // const persons = await Person.find();
+    // for ( i = 0; i < persons.length; i++) {
+    //     const person =  persons[i];
+        var query = { $or: [  
+            { $and: [
+                { firstName: req.body.firstName },
+                { firstName: {$exists: true}} ]},
+            { $and: [
+                { lastName: req.body.lastName},
+                {lastName: {$exists: true}}]},
+            { $and: [
+                { eventName: req.body.eventName},
+                {eventName: {$exists: true}}]},
+            { $and: [
+                { location: req.body.location},
+                {location: {$exists: true}}]},
+            { $and: [
+                { profileUrl: req.body.profileUrl},
+                {profileUrl: {$exists: true}}]},
+            { $and: [
+                { date: req.body.date},
+                {date: {$exists: true}}]},
+            { $and: [
+                { notes: req.body.notes},
+                {notes: {$exists: true}}]}] }
+        await Person.find(query, async function(err, result) {
+        // console.log(result);
+        // res.send('Yay');
+            res.json(result);
+        }, {projection:{ "_id": 0 }})
+    // }
+});
+
 //Adds a person
 router.post('/', async (req, res) => {
     const person = new Person({
@@ -55,16 +89,17 @@ router.delete('/:personId', async (req, res) => {
 //Updates a person's info
 router.patch('/:personId', async (req, res) => {
     try {
+        const person = await Person.findById(req.params.personId);
         const updatedPerson = await Person.updateOne(
             { _id: req.params.personId },
             { $set: { 
-                firstName: (req.body.firstName === null) ? firstName : req.body.firstName,
-                lastName: (req.body.lastName === null) ?  lastName : req.body.lastName,
-                location: (req.body.location === null) ? location : req.body.location,
-                eventName: (req.body.eventName === null) ? eventName : req.body.eventName,
-                date: (req.body.date === null) ? date : req.body.date,
-                profileUrl: (req.body.profileUrl === null) ? profileUrl : req.body.profileUrl,
-                notes: (req.body.notes === null) ? notes : req.body.notes}
+                firstName: (req.body.firstName === undefined) ? person.firstName : req.body.firstName,
+                lastName: (req.body.lastName === undefined) ?  person.lastName : req.body.lastName,
+                location: (req.body.location === undefined) ? person.location : req.body.location,
+                eventName: (req.body.eventName === undefined) ? person.eventName : req.body.eventName,
+                date: (req.body.date === undefined) ? person.date : req.body.date,
+                profileUrl: (req.body.profileUrl === undefined) ? person.profileUrl : req.body.profileUrl,
+                notes: (req.body.notes === undefined) ? person.notes : req.body.notes}
             }
         );
         res.json(updatedPerson);
